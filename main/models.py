d8364b9476802deea7f2305640d118b140b20bb2
RodrigoBorges93 <51095310+RodrigoBorges93@users.noreply.github.com>
2019-11-01 14:59:32 -0300

    Update models.py

--- main/models.py
+++ main/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+# Summary model
 class Summary(models.Model):
     user = models.ForeignKey(User, on_delete = models.PROTECT)
     subject = models.CharField(max_length = 100, help_text = "Enter one or more subjects concerning your summary")
